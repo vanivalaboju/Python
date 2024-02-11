8639193c83e5d040551c827f6d18d3531be13a18
vani_v <kathirojuvani@gmail.com>
2024-02-11 19:54:50 +0530

    sample commit to see push command works or not post repo is made public

--- MyPythonLearnings/Assignments/task_1_02_feb_2024.py
+++ MyPythonLearnings/Assignments/task_1_02_feb_2024.py
@@ -1,7 +1,6 @@
 # Write a python program to calculate the area of a circle given its radius using the formula
 # area = pi*r**2
 
-
 radius = float(input("Enter the radius of the circle:"))
 pi = 3.14
 area = pi * radius ** 2
