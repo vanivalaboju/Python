3ee26a51dd220adbb7eadcb45b1077c10eaaa2f0
vani_v <kathirojuvani@gmail.com>
2024-02-13 23:44:39 +0530

    12 feb task2 added

---
+++ MyPythonLearnings/Assignments/12_feb tsk2_2024.py
@@ -0,0 +1,7 @@
+# Implement a function to calculate the factorial of a number
+
+import math
+def factorial(n):
+    return (math.factorial(n))
+num = int(input("enter the number:"))
+print("Factorial of", num, "is",factorial(num))
